1476994a93bd14dd9bd60007fdf499e32dafb701
DoodleBob <riley.foust18@gmail.com>
2021-09-19 23:42:13 -0400

    finished javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,174 @@
 var startBtn = document.querySelector("#start-button");
 var questionSectionEl = document.querySelector("#question-section");
-var startScreen = document.querySelector("#start-screen");
 var headerEl = document.querySelector("#header");
 var questionEl = document.querySelector("#question");
-var answerBtnEl = document.querySelector("#answer-button");
+var answerBtnEl = document.querySelector("#answer-buttons");
+var endScreen = document.querySelector("#end-screen");
+var quizTimerEl = document.querySelector("#quiz-timer");
+var result = document.querySelector("#end");
+var submitBtn = document.querySelector("#submit");
+var highscoreList = document.querySelector("#highscore-list");
+var initials = document.querySelector("#initials");
 
-var questions = [
-  {
-    question: "Who invented JavaScript?",
-    choices: [
-      {A: "Douglas Crockford"},
-      {B: "Sheryl Sandberg"},
-      {C: "Brendan Eich"}
-      {answer: "C"}
-    ]
+var countdown = 75;
+var currentQuestion = 0;
+var score = 0;
+var newUserId = 0;
+var newScoreId = 0;
+
+var timer = function() {
+  quizTimerEl.textContent = `Time Left: ${Math.max(countdown, 0)}`;
+  countdown--;
+}
+
+var startTimer = function() {
+  setInterval(timer, 1000);
+}
+
+const choices = [
+  { 
+    name: "Hyper Text Markup Language Stand For?",
+    choices: ["JavaScript", "XHTML","CSS", "HTML"],
+    answer: "3"
+  },
+  { 
+    name: "Which language is used for styling web pages?",
+    choices: ["HTML", "JQuery", "CSS", "XML"],
+    answer: "2"
+  },
+  { 
+    name: "Which is not a JavaScript Framework?",
+    choices: ["Python Script", "JQuery","Django", "NodeJS"],
+    answer: "2"
+  },
+  { 
+    name: "Which of these is not a loop type?",
+    choices: ["for", "while", "forEach", "per"],
+    answer: "3"
+  },
+  { 
+    name: "Which element creates a numbered list?",
+    choices: ["ol", "ul", "li", "div"],
+    answer: "0"
   }
-]
+];
 
 var startGame = function() {
-  console.log("started");
+  startTimer();
+  var startScreen = document.querySelector("#start-screen");
   startScreen.classList.add("hide");
   questionSectionEl.classList.remove("hide");
   headerEl.classList.remove("hide");
-  var currentQuestion = 0;
-  newQuestion();
+  questionLoop(choices[currentQuestion].name);
 };
 
 startBtn.addEventListener("click", startGame);
 
-var showQuestion = function(question) {
-
+var questionLoop = function(question) {
+  questionEl.textContent = question;
+  createAnswers(choices[currentQuestion].choices);
+  answerBtnEl.addEventListener("click", pickedAsnwer);
 }
 
-var newQuestion = function() {
-  showQuestion(questions[currentQuestion]);
-};
+var createAnswers = function(answers) {
+  for (var i = 0; i < answers.length; i++) {
+    var newAnswer = document.querySelector("#btn");
+    newAnswer.className = "btn"
+    newAnswer.setAttribute("btn-id", i);
+    newAnswer.textContent = answers[i]
+    answerBtnEl.appendChild(newAnswer);
+  }
+}
 
-var answer = function() {
+var pickedAsnwer = function(event) {
+  if (event.target.matches(".btn")) {
+    var correctAnswer = event.target.getAttribute("btn-id");
+    if (correctAnswer === choices[currentQuestion].answer) {
+      currentQuestion++;
+      rightAnswer();
+    }
+    else {
+      currentQuestion++;
+      wrongAnswer();
+    }
+  }
+}
 
-};
+var rightAnswer = function() {
+  if (currentQuestion === choices.length || countdown <= 0) {
+    endQuiz();
+  }
+  else {
+    questionLoop(choices[currentQuestion].name);
+        result.textContent = "You got it!";
+  }
+}
+
+var wrongAnswer = function() {
+  if (currentQuestion === choices.length || countdown <= 0) {
+    endQuiz();
+  }
+  else {
+    questionLoop(choices[currentQuestion].name);
+        result.textContent = "Uh oh wrong one!"
+        countdown -= 15;
+  }
+}
+
+var storedScores = JSON.parse(localStorage.getItem("Highscores"));
+var scoreList = []
+
+var endQuiz = function() {
+  headerEl.className = "hide"
+  endScreen.classList.remove("hide");
+  var endText = document.querySelector("#end-text");
+  questionSectionEl.classList.add("hide");
+  endText.textContent = "All done!";
+  var endScore = document.querySelector("#end-score");
+  endScore.textContent = `Your score: ${Math.max(countdown, 0)}`;
+  score = Math.max(countdown, 0);
+  submitBtn.addEventListener("click", function() {
+    if (initials.value === "") {
+      alert("Please enter your initials.")
+    } 
+    else if (initials.value.length > 2) {
+      alert("Please use only 2 letters")
+    }
+    else {
+      var userScore = `${initials.value} ${score}`
+      // if (storedScores === null) {
+        scoreList.push(userScore)
+        localStorage.setItem("Highscores", JSON.stringify(scoreList));
+        scoreList.push(storedScores)
+        console.log(scoreList)
+        // var test = JSON.parse(JSON.stringify(scoreList))
+        // test.push('qq 31')
+        // console.log(test)
+        // localStorage.setItem("Highscores", JSON.stringify(test));
+        // console.log(storedScores)
+        // test.push("tt 22")
+        // console.log(test)
+        // console.log(userScore)
+        // viewHighscores();
+      // }
+      // else {
+      //   storedScores.push(userScore)
+      //   localStorage.setItem("Highscores", JSON.stringify(storedScores));
+      //   viewHighscores();
+      // }
+    }
+  });
+}
+
+// var viewHighscores = function() {
+//   endScreen.className = "hide"
+//   headerEl.className = "hide"
+//   console.log(scoreList)
+//   // for (var i = 0; i < storedScores.length; i++) {
+//   //   var scoreListEl = document.createElement("li");
+//   //   var br = document.createElement("br")
+//   //   scoreListEl.textContent = storedScores[i];
+//   //   highscoreList.appendChild(scoreListEl);
+//   //   highscoreList.appendChild(br)
+//   // }
+// }
